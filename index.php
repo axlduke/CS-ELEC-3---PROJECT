0199cb93b4ff7a4991815d3ed0859df9f6577948
axlduke <63648856+axlduke@users.noreply.github.com>
2022-07-13 21:53:53 +0800

    template for search

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Project</title>
+</head>
+<body class="bg-slate-500">
+    <nav class="items-center bg-slate-800 w-full py-3 px-10">
+        <div class="flex justify-between">
+            <div>
+                <h1 class="text-white text-2xl">Project</h1>
+            </div>
+            <div>
+                <input type="text" class="py-2 ease-in-out duration-300 rounded-full pl-5 text-black outline-0 border-4 border-orange-400 w-72">
+                <i class="uil uil-search text-white text-2xl"></i>
+            </div>
+            <div>
+                <a href="">
+                    <i class="uil uil-user-square text-white text-2xl hover:text-orange-400 ease-in-out duration-300"></i>
+                </a>
+                <a href="">
+                    <i class="uil uil-signin text-white text-2xl hover:text-orange-400 ease-in-out duration-300"></i>
+                </a>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
