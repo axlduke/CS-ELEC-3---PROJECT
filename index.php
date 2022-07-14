c9c52dde7624c167967e0dcfd0df30ce8942b11d
axlduke <63648856+axlduke@users.noreply.github.com>
2022-07-14 21:47:31 +0800

    need to update

--- index.php
+++ index.php
@@ -7,27 +7,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
     <title>Project</title>
 </head>
-<body class="bg-slate-500">
-    <nav class="items-center bg-slate-800 w-full py-3 px-10">
-        <div class="flex justify-between">
+<body>
+    <div id="loader" class="building-blocks">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+    <nav id="content" class="bg-slate-800 w-full py-3 px-10">
+        <div class="flex justify-between items-center bg-slate-800 w-full py-3 px-10">
             <div>
                 <h1 class="text-white text-2xl">Project</h1>
             </div>
             <div>
-                <input type="text" class="py-2 ease-in-out duration-300 rounded-full pl-5 text-black outline-0 border-4 border-orange-400 w-72">
-                <i class="uil uil-search text-white text-2xl"></i>
+                <form action="" method="post">
+                    <input type="text" class="py-2 ease-in-out duration-300 rounded-full pl-5 text-black outline-0 border-4 border-[#46E0EA] w-72">
+                    <i class="uil uil-search text-white text-2xl"></i>
+                </form>
             </div>
             <div>
                 <a href="">
-                    <i class="uil uil-user-square text-white text-2xl hover:text-orange-400 ease-in-out duration-300"></i>
-                </a>
-                <a href="">
-                    <i class="uil uil-signin text-white text-2xl hover:text-orange-400 ease-in-out duration-300"></i>
+                    <i class="uil uil-signin text-white text-2xl hover:text-[#46E0EA] ease-in-out duration-300">Log In</i>
                 </a>
             </div>
         </div>
     </nav>
+    <section class="flex bg-gray-600 p-10 h-[40.6rem]">
+        <div class="items-center">
+            
+        </div>
+    </section>
 </body>
 </html>
